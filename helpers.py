7af928031de0e3e5d7283e3cd5b7c6d307bdf449
Kazesui <tomkul@hotmail.com>
2019-11-19 02:24:57 +0100

    Added some more content

--- helpers.py
+++ helpers.py
@@ -14,5 +14,11 @@ def load_binary(filename, **kwargs):
     return data
 
 
+# Quick and dirty way of converting our string labels to numbers for conversion to 1-hot encoded vectors
+def train_to_id(train_type):
+    tt_lut = {'train_a': 0, 'train_b': 1, 'train_c': 2, 'train_d': 3, 'unknown': 4}
+    return tt_lut[train_type]
+
+
 def plot_size(width, height):
     matplotlib.rcParams['figure.figsize'] = width, height
